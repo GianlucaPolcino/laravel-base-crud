97875df27c62a2d9d48c18ef492da57e21b2633d
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2022-01-28 19:51:35 +0100

    crud index done

--- resources/views/comics/home.blade.php
+++ resources/views/comics/home.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>
+<h1 class="text-center pt-5">
     Pagina comics
 </h1>
 
-@foreach ($comics as $comic)
-<img src="{{$comic->thumb}}" alt="">    
-@endforeach
+<div class="row py-5">
+    @foreach ($comics as $comic)
+    <div class="col">
+        <img class="w-100" src="{{$comic->thumb}}" alt="">
+        <h4 class="text-center pt-3">
+            {{$comic->title}}
+        </h4>
+        
+        <div class="text-center">
+            <button class="btn btn-primary mt-4">Vedi</button>
+        </div>
+        
+    </div>
+    
+    @endforeach
+</div>
+
+<div class="d-flex justify-content-center">
+    {{$comics->links('pagination::bootstrap-4')}}
+</div>
 @endsection
